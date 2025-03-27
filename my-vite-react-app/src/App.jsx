12dbf6de1f60e72522da822b22f6b782cf350673
PS-Harshil-Suthar <Harshil.Suthar@productsquads.co>
2025-03-28 00:10:04 +0530

    First Commit

---
+++ my-vite-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Greeting from './Greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, Welcome to My Vite React App!</h1>
+      <Greeting name="User" />
+    </div>
+  );
+}
+
+export default App;
